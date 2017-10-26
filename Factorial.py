25c1cb5955c4b9718d5f403864816db8ca97c605
Oleksandr Padko <sasha.pad@gmail.com>
2017-10-26 11:43:37 +0300

    Encapsulate factorial into a function

--- Factorial.py
+++ Factorial.py
@@ -6,14 +6,16 @@ num = 7
 # uncomment to take input from the user
 #num = int(input("Enter a number: "))
 
-factorial = 1
+def fact(n):
+	# check if the number is negative, positive or zero
+	if num < 0:
+	   print("Sorry, factorial does not exist for negative numbers")
+	elif num == 0:
+	   print("The factorial of 0 is 1")
+	else:
+	   factorial = 1
+	   for i in range(1,num + 1):
+	       factorial = factorial*i
+	   print("The factorial of",num,"is",factorial)
 
-# check if the number is negative, positive or zero
-if num < 0:
-   print("Sorry, factorial does not exist for negative numbers")
-elif num == 0:
-   print("The factorial of 0 is 1")
-else:
-   for i in range(1,num + 1):
-       factorial = factorial*i
-   print("The factorial of",num,"is",factorial)
+fact(num)
